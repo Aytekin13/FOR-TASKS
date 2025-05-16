b5be45f7fe36617b33e1b77d6e1db9c3e025c764
Aytekin13 <mrsyt@gmail.com>
2025-05-16 21:53:51 +0400

    Add project files.

---
+++ FOR TASKS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOR_TASKS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
+
+
+  
+
+
